f6488dfa8c7d9c71aa7fc5d4717f65a00bb005b7
WujuMaster <sbach2411@gmail.com>
2021-11-05 11:53:18 +0700

    new commit

---
+++ functions.py
@@ -0,0 +1,108 @@
+import cv2
+import face_recognition
+import pandas as pd
+import os
+import numpy as np
+import time
+
+def check_savefile(savefile):
+    if not os.path.isfile(savefile):
+        df = pd.DataFrame(list())
+        df.to_csv(savefile)
+
+def save_face(picpath, savefile, name='unknown'):
+    input = face_recognition.load_image_file(picpath)
+    input = cv2.cvtColor(input, cv2.COLOR_BGR2RGB)
+
+    faceLoc = face_recognition.face_locations(input)[0]
+    encoded = face_recognition.face_encodings(input)[0]
+
+    start_point = (faceLoc[3], faceLoc[0])
+    end_point = (faceLoc[1], faceLoc[2])
+
+    data = pd.DataFrame([[name, start_point, end_point, encoded]])
+    
+    check_savefile(savefile)
+    
+    df = pd.read_csv(savefile)
+    if len(df.axes[0]) < 1:
+        data.to_csv(savefile, index=False, header=['Name', 'Start_point', 'End_point', 'Features'])
+    else:
+        data.to_csv(savefile, mode='a', index=False, header=None)        
+    print('Saved!')
+
+def save_to_db(pic, db_file):
+    answer = input('Do you want to save new data? (Y/n) ').lower()
+    if answer==('y') or answer==('yes'):
+        save_face(pic, db_file, name=input('Enter name to save: '))
+        return True
+    else:
+        return False
+
+def verify_face(picpath, savefile):
+    if isinstance(picpath, np.ndarray):
+        input = cv2.cvtColor(picpath, cv2.COLOR_BGR2RGB)
+    else:
+        input = face_recognition.load_image_file(picpath)
+        input = cv2.cvtColor(input, cv2.COLOR_BGR2RGB)
+
+    encoded = face_recognition.face_encodings(input)[0]
+    locations = face_recognition.face_locations(input)[0]
+    # print(type(locations))
+
+    df = pd.read_csv(savefile)
+    features = df.iloc[:, 3].values
+    names = df.iloc[:, 0].values
+
+    id = ''
+    min = 5
+    for i in range(len(df.axes[0])):
+        features[i] = features[i][1:-1]
+        face = np.fromstring(features[i], sep=' ')
+        result = face_recognition.compare_faces([face], encoded)
+        distance = face_recognition.face_distance([face], encoded)
+        if result[0]==True:
+            if min > distance and distance<=0.5:
+                min = distance
+                id = names[i] 
+    return id, locations
+
+def cam_capture(savefile):
+    vid = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    # pTime = 0
+    while True:
+        _ , frame = vid.read()
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        rgb_small_frame = small_frame[:, :, ::-1]
+        try:
+            name, locations = verify_face(rgb_small_frame, savefile)
+            # name, locations = verify_face(frame, savefile)
+        except:
+            name = ''
+            locations = ()
+            
+        if len(name)<1:
+            name = 'unknown'
+
+        #start_point is top left, end_point is below right
+        if len(locations) > 0:
+            # cv2.rectangle(frame, (locations[3], locations[0]), (locations[1], locations[2]), (255, 0, 0), 2)
+            cv2.rectangle(frame, (locations[3]*4, locations[0]*4), (locations[1]*4, locations[2]*4), (255, 0, 0), 2)
+            font                   = cv2.FONT_HERSHEY_SIMPLEX
+            bottomLeftCornerOfText = (locations[3]*4, locations[0]*4)
+            fontScale              = 1
+            fontColor              = (255,255,255)
+            lineType               = 2
+
+            cv2.putText(frame, name, bottomLeftCornerOfText, font, fontScale, fontColor, lineType)
+
+        # cTime = time.time()
+        # fps = 1/(cTime-pTime)
+        # pTime = cTime
+
+        cv2.imshow('detecting', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    vid.release()
+    cv2.destroyAllWindows()
---
+++ main.py
@@ -0,0 +1,43 @@
+# import cv2
+# import numpy as np
+# import face_recognition
+# import pandas as pd
+import os
+from functions import cam_capture
+# from functions import verify_face
+from functions import save_to_db
+
+db_file = './FaceDatabase.csv'
+pictures = './Pictures/'
+
+# save_face(os.path.join(pictures, 'joji.jpg'), db_file, 'Joji')
+# save_face(os.path.join(pictures, 'max.jpg'), db_file, 'Maxmoefoe')
+# save_face(os.path.join(pictures, 'pinkguy.jpg'), db_file, 'Pink Guy')
+# verify_face(os.path.join(pictures, 'SalamanderMan.jpg'), db_file)
+# verify_face(os.path.join(pictures, 'iDubbbz.jpg'), db_file)
+
+menu = {}
+menu['1'] = "Create new faceID" 
+menu['2'] = "Log in with Camera"
+menu['3'] = "Exit"
+while True: 
+    options = sorted(menu)
+    for entry in options: 
+        print(entry, menu[entry])
+
+    selection = input("Option: ") 
+    if selection =='1': 
+        pic = input('Select picture: ')
+        pic = os.path.join(pictures, pic + '.jpg')
+        res = save_to_db(pic, db_file)
+        if res:
+            print('Saved successfully!')
+        else:
+            print('Cancelled!')
+    elif selection == '2':
+        cam_capture(db_file)
+    elif selection == '3': 
+        break
+    else:
+        print("Unknown Option Selected!")
+    print('======')
